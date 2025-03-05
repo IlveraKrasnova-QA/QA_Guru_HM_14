ec43eb6e27bc7451cf7b972089b879da1ffa5a1f
krasnova.ilvera <ilvera.krasnova@yandex.ru>
2025-03-05 23:38:58 +0300

    added tests

---
+++ src/test/java/TestBase.java
@@ -0,0 +1,12 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+
+public class TestBase {
+        @BeforeAll
+        static void beforeAll(){
+        Configuration.baseUrl = "https://www.avito.ru/";
+        Configuration.browserSize = "1928x1080";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
+    }
+}
