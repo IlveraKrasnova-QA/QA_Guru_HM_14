b0581742549a06de08d5109d392b35c512ef4ba2
krasnova.ilvera <ilvera.krasnova@yandex.ru>
2025-03-19 01:44:51 +0300

    added tests

--- src/test/java/tests/NewFlatsJUnitTests.java
+++ src/test/java/tests/NewFlatsJUnitTests.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import pages.NewFlatsPage;
 
+import static io.qameta.allure.Allure.step;
+
 public class NewFlatsJUnitTests extends TestBase {
 
     NewFlatsPage newFlatsPage = new NewFlatsPage();
@@ -11,48 +13,66 @@ public class NewFlatsJUnitTests extends TestBase {
     @Test
     @DisplayName("Переход на страницу новостроек через  шапку сайта")
     void successfulRedirectToNewBuildingsFromHeaderTest() {
+        step("Кликаем на каталог новостроек в навбаре", () -> {
         newFlatsPage.openMainPage()
-                    .navBarSection("Каталоги")
-                    .redirectToSectionFromNavBar()
+                    .navBarSection("Каталоги");
+        });
+        step("Переходим на страницу с новостройками", () -> {
+        newFlatsPage.redirectToSectionFromNavBar()
                     .checkTitle("Новостройки");
+        });
 
     }
 
     @Test
     @DisplayName("Переход на страницу новостроек через поиск")
     void successfulRedirectToNewBuildingsFromCategoriesTest() {
+        step("Вводим в поиске название категории и переходим по результату", () -> {
         newFlatsPage.openMainPage()
-                    .redirectToSectionFromSearch("новостройки")
-                    .checkPageTitle("новостройки")
+                    .redirectToSectionFromSearch("новостройки");
+        });
+        step("Проверяем отображение корректного заголовка и 'хлебных крошек'", () -> {
+        newFlatsPage.checkPageTitle("новостройки")
                     .checkBreadcrumbs("новостройки");
+        });
 
     }
 
     @Test
     @DisplayName("Проверка отображения новостроек для опреденного города")
     void successRegionChangingTest() {
+        step("Открываем попап с выбором города и региона", () -> {
         newFlatsPage.openMainPage()
                     .navBarSection("Каталоги")
                     .redirectToSectionFromNavBar()
                     .openMap()
-                    .checkMapTitle("Город или регион")
-                    .clickOnMapSearch()
+                    .checkMapTitle("Город или регион");
+        });
+        step("Выбираем город", () -> {
+        newFlatsPage.clickOnMapSearch()
                     .setCity("Казань")
                     .clickOnCity("Казань")
-                    .showResults()
-                    .checkCityResults("Квартиры в новых ЖК в Казани");
+                    .showResults();
+        });
+        step("Проверяем отображение корректных результатов", () -> {
+            newFlatsPage.checkCityResults("Квартиры в новых ЖК в Казани");
+        });
 
     }
 
     @Test
     @DisplayName("Проверка отображения подсказки по выбору адреса")
     void chekingCalculatorTest() {
+        step("Переходим в раздел новостроек", () -> {
         newFlatsPage.openMainPage()
                     .navBarSection("Каталоги")
-                    .redirectToSectionFromNavBar()
-                    .scrollToTooltip("Где искать")
+                    .redirectToSectionFromNavBar();
+        });
+        step("Проверяем отображение тултипа", () -> {
+        newFlatsPage.scrollToTooltip("Где искать")
                     .checkTooltip()
                     .checkTooltipText("Город и регион можно выбрать тут, а ещё искать по районам и адресам");
+        });
 
     }
 
--- src/test/java/tests/RegistrationFormJUnitTests.java
+++ src/test/java/tests/RegistrationFormJUnitTests.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import pages.RegistrationFormPage;
 
+import static io.qameta.allure.Allure.step;
+
 public class RegistrationFormJUnitTests extends TestBase {
 
     RegistrationFormPage registrationFormPage = new RegistrationFormPage();
@@ -11,45 +13,73 @@ public class RegistrationFormJUnitTests extends TestBase {
     @Test
     @DisplayName("Неуспешная регистрация без ввода номера телефона")
     void unsuccessfulRegistrationWithoutNumberTest() {
+        step("Открываем форму входа", () -> {
         registrationFormPage.openMainPage()
-                            .openRegisrationForm("Вход и регистрация")
-                            .clickOnRegistrationButton()
-                            .chooseAccountType()
-                            .clickOnContinueButton()
-                            .checkWarning("Заполните поле");
+                            .openRegisrationForm("Вход и регистрация");
+        });
+        step("Переходим на экран выбора типа нового аккаунта", () -> {
+        registrationFormPage.clickOnRegistrationButton();
+        });
+        step("Выбираем личный тип аккаунта для регистрации", () -> {
+        registrationFormPage.chooseAccountType();
+        });
+        step("Нажимаем на кнопку 'Продолжить' без ввода номера телефона", () -> {
+        registrationFormPage.clickOnContinueButton();
+        });
+        step("Проверяем появление ошибки", () -> {
+        registrationFormPage.checkWarning("Заполните поле");
+        });
     }
 
     @Test
     @DisplayName("Проверяем редирект на форму входа через форму регистрации")
     void redirectToLoginFromRegistrationFormTest() {
+        step("Открываем форму входа", () -> {
         registrationFormPage.openMainPage()
-                            .openRegisrationForm("Вход и регистрация")
-                            .clickOnRegistrationButton()
-                            .chooseAccountType()
-                            .checkText("Уже есть профиль? ")
-                            .checkText("Войти")
-                            .clickEntryButton()
+                            .openRegisrationForm("Вход и регистрация");
+        });
+        step("Переходим на экран выбора типа нового аккаунта", () -> {
+        registrationFormPage.clickOnRegistrationButton();
+        });
+        step("Выбираем личный тип аккаунта для регистрации", () -> {
+            registrationFormPage.chooseAccountType();
+        });
+        step("Проверяем отображение кнопки 'Войти' на форме регистрации", () -> {
+        registrationFormPage.checkText("Уже есть профиль? ")
+                            .checkText("Войти");
+        });
+        step("Переходим на форму входа", () -> {
+        registrationFormPage.clickEntryButton()
                             .entryForm();
+        });
 
     }
 
     @Test
     @DisplayName("Проверяем редирект на условия пользования")
     void termsOfUseTest() {
+        step("Открываем форму входа", () -> {
         registrationFormPage.openMainPage()
-                            .openRegisrationForm("Вход и регистрация")
-                            .redirectToSection("условиями использования Авито")
+                            .openRegisrationForm("Вход и регистрация");
+        });
+        step("Переходим на страницу с условиями пользования", () -> {
+        registrationFormPage.redirectToSection("условиями использования Авито")
                             .checkPageTitle("Условия использования Авито");
+        });
 
     }
 
     @Test
     @DisplayName("Проверяем редирект на политику конфиденциальности")
     void  privacyPolicyTest() {
+        step("Открываем форму входа", () -> {
         registrationFormPage.openMainPage()
-                            .openRegisrationForm("Вход и регистрация")
-                            .redirectToSection("политикой конфиденциальности")
+                                .openRegisrationForm("Вход и регистрация");
+        });
+        step("Переходим на страницу с политикой конфиденциальности", () -> {
+        registrationFormPage.redirectToSection("политикой конфиденциальности")
                             .checkPageTitle("Политика конфиденциальности");
+        });
 
     }
 
