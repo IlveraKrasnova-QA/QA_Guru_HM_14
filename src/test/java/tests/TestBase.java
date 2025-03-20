1f2803f5ea57573da54881fe24509a53cfcfdff7
krasnova.ilvera <ilvera.krasnova@yandex.ru>
2025-03-21 00:36:28 +0300

    added tests

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -43,6 +43,7 @@ public class TestBase {
                 Attach.pageSource();
                 Attach.browserConsoleLogs();
                 Attach.addVideo();
+
                 Selenide.closeWebDriver();
         }
 }
